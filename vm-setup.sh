8eb668e2b9a339dd4347aea4648422e56b52b698
seenfan <seen.ys.fan@mail.foxconn.com>
2018-01-05 14:01:54 +0800

    No need to make file system

--- vm-setup.sh
+++ vm-setup.sh
@@ -18,7 +18,6 @@ p
 
 w
 EOF
-mkfs.ext4 /dev/vdb1
 echo "Now test"
 echo "Test sequential write"
 /usr/local/bin/fio -filename=/dev/vdb1 -direct=1 -iodepth 1 -thread -rw=write -ioengine=psync -bs=16k -size=200G -numjobs=30 -runtime=1000 -name=jicki
