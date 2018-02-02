e4c52aeceed75042d0e0e2ce33fac113164d318f
seenfan <seen.ys.fan@mail.foxconn.com>
2018-02-02 16:24:59 +0800

    Change subnet

--- openstack-network-setup.sh
+++ openstack-network-setup.sh
@@ -3,7 +3,7 @@
 echo "Create router"
 openstack router create router
 echo "Create provider network"
-./esmsagent.py --createprovidernetwork 192.168.2.0/24 192.168.2.101 192.168.2.150 192.168.2.200 8.8.8.8
+./esmsagent.py --createprovidernetwork 192.168.127.0/24 192.168.127.101 192.168.127.150 192.168.127.200 8.8.8.8
 echo "Create private nework"
 openstack network create private_network
 openstack subnet create --network private_network --subnet-range 192.168.3.0/24 --gateway 192.168.3.1 private_subnet
