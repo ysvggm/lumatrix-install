5c4d487d183eba72fd773eca4af5039f73ab4022
seenfan <seen.ys.fan@mail.foxconn.com>
2018-03-29 11:31:34 +0800

    Modified for virtual box VM

--- install-openstack.sh
+++ install-openstack.sh
@@ -9,12 +9,12 @@ sed -i 's/$db_sync_timeout = 300/$db_sync_timeout = 1200/' /usr/share/openstack-
 sed -i 's/$db_sync_timeout = 300/$db_sync_timeout = 1200/' /usr/share/openstack-puppet/modules/neutron/manifests/db/sync.pp
 packstack --answer-file=./packstack-answers.txt 
 echo "Copy network scripts"
-cp /etc/sysconfig/network-scripts/ifcfg-enp5s0 /root/ifcfg-enp5s0.bkp
-cp ifcfg-enp5s0 /etc/sysconfig/network-scripts/
+cp /etc/sysconfig/network-scripts/ifcfg-enp0s3 /root/ifcfg-enp0s3.bkp
+cp ifcfg-enp0s3 /etc/sysconfig/network-scripts/
 cp ifcfg-br-ex /etc/sysconfig/network-scripts/
-ifdown ifcfg-enp5s0
+ifdown ifcfg-enp0s3
 ifdown br-ex
-ifup ifcfg-enp5s0
+ifup ifcfg-enp0s3
 ifup br-ex
 systemctl restart openvswitch.service
 mv /etc/httpd/conf.d/15-default.conf /etc/httpd/conf.d/15-default.conf.bkp
@@ -22,4 +22,4 @@ sed -i 's/ServerAlias 127.0.0.1/## ServerAlias 127.0.0.1/' /etc/httpd/conf.d/15-
 sed -i 's/ServerAlias ceph-node3/## ServerAlias ceph-node3/' /etc/httpd/conf.d/15-horizon_vhost.conf
 sed -i 's/ServerAlias localhost/## ServerAlias localhost/' /etc/httpd/conf.d/15-horizon_vhost.conf
 systemctl restart httpd.service
-ip route replace default via 192.168.127.101 dev br-ex
+ip route replace default via 192.168.42.129 dev br-ex
